c37e05371ff7aae33ae100ac65905c54135a17d7
blackutau <teresafloresc@hotmail.com>
2019-09-24 17:30:01 -0500

    Implementar clase categoria

--- src/main/java/com/hampcode/model/entity/Categoria.java
+++ src/main/java/com/hampcode/model/entity/Categoria.java
@@ -1,5 +1,42 @@
 package com.hampcode.model.entity;
 
-public class Categoria {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.ws.rs.DefaultValue;
 
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+	
+	@Id
+	@Column(name = "codigo")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@Column(name = "nombre", nullable = false, length = 100)
+	@DefaultValue(value = "")
+	private String nombre;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 }
+
