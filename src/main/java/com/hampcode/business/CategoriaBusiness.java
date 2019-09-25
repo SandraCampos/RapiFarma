f9382cb649ae0ac4af222334d0a3c341c5a351ea
blackutau <teresafloresc@hotmail.com>
2019-09-24 23:06:56 -0500

    Implementacion categoriabusiness

--- src/main/java/com/hampcode/business/CategoriaBusiness.java
+++ src/main/java/com/hampcode/business/CategoriaBusiness.java
@@ -1,5 +1,39 @@
 package com.hampcode.business;
 
-public class CategoriaBusiness {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+import com.hampcode.model.entity.Categoria;
+import com.hampcode.model.repository.CategoriaRepository;
+
+@Named
+public class CategoriaBusiness implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private CategoriaRepository categoriaRepository;
+	
+	@Transactional
+	public Long registrar(Categoria categoria) throws Exception{
+		return this.categoriaRepository.registrar(categoria);
+	}
+	
+	@Transactional
+	public Long modificar(Categoria categoria) throws Exception{
+		return this.categoriaRepository.modificar(categoria);
+	}
+	
+	@Transactional
+	public Long eliminar(Categoria categoria) throws Exception{
+		return this.categoriaRepository.eliminar(categoria);
+	}
+	
+	public List<Categoria> listarTodo() throws Exception{
+		return this.categoriaRepository.listarTodo();
+	}
 }
