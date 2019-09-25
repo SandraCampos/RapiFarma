c717b4c33ece2d700e13e92b63d96d0edd620cd1
blackutau <teresafloresc@hotmail.com>
2019-09-24 23:03:21 -0500

    implementacion categoriarepository

--- src/main/java/com/hampcode/model/repository/CategoriaRepository.java
+++ src/main/java/com/hampcode/model/repository/CategoriaRepository.java
@@ -1,5 +1,41 @@
 package com.hampcode.model.repository;
 
-public class CategoriaRepository {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import com.hampcode.model.entity.Categoria;
+
+@Named
+public class CategoriaRepository implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@PersistenceContext(unitName="pwPU")
+	private EntityManager em;
+	
+	public Long registrar(Categoria categoria) throws Exception{
+		this.em.persist(categoria);
+		return categoria.getCodigo();
+	}
+	
+	public Long modificar(Categoria categoria) throws Exception{
+		this.em.merge(categoria);
+		return categoria.getCodigo();
+	}
+	
+	public Long eliminar(Categoria categoria) throws Exception{
+		this.em.remove(categoria);
+		return categoria.getCodigo();
+	}
+	
+	public List<Categoria> listarTodo() throws Exception{
+		TypedQuery<Categoria> query = this.em.createQuery("FROM Categoria c",
+				Categoria.class);
+		return query.getResultList();
+	}
 }
